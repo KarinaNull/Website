16060a34f2bec0c76e2788cf3e6ae688ae6151a1
KarinaNull <Karina6663831@gmail.com>
2021-06-08 01:15:47 +0300

    Add random quote display

--- index.php
+++ index.php
@@ -52,53 +52,20 @@
         <section>
             <blockquote>
                 <p>
-                    "Нельзя сказать человеку: «Ты можешь творить.
-                    Так давай, твори». Гораздо вернее подождать, пока он
-                    сам не скажет: «Я могу творить, и я буду творить, хотите
-                    вы этого или нет»"
+                    <?php
+                    $quotes = file('quotes.txt');
+                    $randomQuote = $quotes[rand(0, count($quotes) - 1)];
+                    echo $randomQuote;
+                    ?>
                 </p>
                 <cite>John Doe, Lorem Ipsum</cite>
             </blockquote>
             <div class="Section-p">
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                    classical
-                    Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from
-                    a
-                    Lorem
-                    Ipsum passage, and going through the cites of the word in classical literature, discovered the
-                    undoubtable
-                    source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
-                    Extremes
-                    of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very
-                    popular
-                    during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a
-                    line
-                    in
-                    section 1.10.32.</p>
-
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when
-                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                    distribution
-                    of letters, as opposed to using 'Content here, content here', making it look like readable English.
-                    Many
-                    desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
-                    and a
-                    search for 'lorem ipsum' will uncover many web sites still in their infancy.</p>
-
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                    alteration
-                    in some form, by injected humour, or randomised words which don't look even slightly believable. If
-                    you
-                    are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing
-                    hidden
-                    in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined
-                    chunks
-                    as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200
-                    Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which
-                    looks
-                    reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or
-                    non-characteristic words etc.</p>
+                <p> Текст (от лат. textus — ткань; сплетение, сочетание) — зафиксированная на каком-либо материальном носителе человеческая мысль; в общем плане связная и полная последовательность символов.
+                    Существуют две основные трактовки понятия «текст»: имманентная (расширенная, философски нагруженная) и репрезентативная (более частная). Имманентный подход подразумевает отношение к тексту как к автономной реальности, нацеленность на выявление его внутренней структуры. Репрезентативный — рассмотрение текста как особой формы представления информации о внешней тексту действительности.
+                    В лингвистике термин «текст» используется в широком значении, включая и образцы устной речи. Восприятие текста изучается в рамках лингвистики текста и психолингвистики. Так, например, И. Р. Гальперин определяет текст следующим образом: «Это письменное сообщение, объективированное в виде письменного документа, состоящее из ряда высказываний, объединённых разными типами лексической,
+                    грамматической и логической связи, имеющее определённый моральный характер, прагматическую установку и соответственно литературно обработанное»[1].
+                </p>
             </div>
             <figure>
                 <img src="#" width="320" height="175" alt="">
